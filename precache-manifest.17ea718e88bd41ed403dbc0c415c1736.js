e0cd0613b6fb97e69c83ea421788f44aaa86aea1
Kevin Perez <kevinperez996@outlook.com>
2020-01-11 14:31:14 -0500

    Updates

---
+++ precache-manifest.17ea718e88bd41ed403dbc0c415c1736.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6001355b91eb5c22db06aff815bd13e8",
+    "url": "/Click-Game/index.html"
+  },
+  {
+    "revision": "042a2ebcccb8bf4eabdd",
+    "url": "/Click-Game/static/css/main.8cac1329.chunk.css"
+  },
+  {
+    "revision": "c2b72a32f5838bbe027c",
+    "url": "/Click-Game/static/js/2.8b5796ad.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Click-Game/static/js/2.8b5796ad.chunk.js.LICENSE"
+  },
+  {
+    "revision": "042a2ebcccb8bf4eabdd",
+    "url": "/Click-Game/static/js/main.f45681c8.chunk.js"
+  },
+  {
+    "revision": "5f98cfcf983f5212c870",
+    "url": "/Click-Game/static/js/runtime-main.fe591509.js"
+  }
+]);
